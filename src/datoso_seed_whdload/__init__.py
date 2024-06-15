23efc284c176babb549c022c774be2fb9407e6f8
laromicas <laromicas@hotmail.com>
2024-06-14 22:37:29 -0500

    Update datoso_seed_whdload version

--- src/datoso_seed_whdload/__init__.py
+++ src/datoso_seed_whdload/__init__.py
@@ -1,6 +1,6 @@
 """Initialize seed variables."""
 __all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0b0.dev0'
+__version__ = '1.0.0rc1'
 __author__ = 'Lacides Miranda'
 __description__ = 'WHDLoad is a collection of Amiga Roms'
 __prefix__ = 'whdload'
