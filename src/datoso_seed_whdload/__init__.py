764041dbcbbe6a44500ff4742d33c1589003d75e
laromicas <laromicas@hotmail.com>
2025-05-12 23:52:29 -0500

    - Bump version - Ruff code changes

--- src/datoso_seed_whdload/__init__.py
+++ src/datoso_seed_whdload/__init__.py
@@ -1,6 +1,6 @@
 """Initialize seed variables."""
-__all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0.0'
+__all__ = ['__author__', '__description__', '__version__']
+__version__ = '1.0.1'
 __author__ = 'Lacides Miranda'
 __description__ = 'WHDLoad is a collection of Amiga Roms'
 __prefix__ = 'whdload'
